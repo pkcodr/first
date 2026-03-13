a263254b47422322b0fc812e4f528eaedc853cc3
pkcodr <studytime035@gmail.com>
2026-03-13 16:34:46 +0530

    Update arrayofstructure.c
    this is crickter name strorage for

--- arrayofstructure.c
+++ arrayofstructure.c
@@ -1,1 +1,32 @@
-
+// Online C compiler to run C program online
+#include <stdio.h>
+struct cricket{
+  char name[20];
+  int age;
+  int nom;
+  float avgrun;
+};
+struct cricket p1;
+int main() {
+    int i;
+    for(i=0;i<2;i++){
+        
+        printf("type name of cricketer : ");
+        scanf("%s",&p1.name);
+        printf("type the age of cricketer :");
+        scanf("%d",&p1.age);
+         printf("type the number of match :");
+         scanf("%d",&p1.nom);
+         printf("type the avgrun :");
+         scanf("%d",&p1.avgrun);
+         
+    }
+  for(int i=0;i<2;i++){
+      printf("Name = %s\n",p1.name);
+       printf("age = %d\n",p1.age);
+        printf("number of match played = %d\n",p1.nom);
+         printf("average run = %d\n",p1.avgrun);
+  }
+   
+    return 0;
+}
